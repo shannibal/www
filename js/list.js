71817d0afc1ec7cd3de9aa3f28272b1b12955b14
Stefan Hannibal <hannibalstefan@gmail.com>
2017-01-30 22:19:28 +0100

    java script files

---
+++ js/list.js
@@ -0,0 +1,26 @@
+function test() {
+    $.ajax({
+        type: "post", dataType: "json",
+        cache: false, async: true,
+        url: "/php/ajax.php",
+        data: {'test': 'test fucker'},
+        success: function(response) {
+            console.log(response);
+            $.each(response, function(i, val) {
+                if (val.meso_id != null) {
+                    if (!$("div.macro#" + val.macro_id).length) {
+                        $("div.test").append("<div class='macro' id='" + val.macro_id + "'>Month:" + val.macro_id +"</div>");    
+                    }
+                    $("div.macro#" + val.macro_id).append("<div class='meso'>" + val.meso_id + "</div>");
+                } else {
+                    if (!$("div.macro#" + val.macro_id).length) {
+                        $("div.test").append("<div class='macro' id='" + val.macro_id + "'>Month:" + val.macro_id +"</div>");    
+                    }
+                }
+            });
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
+}
