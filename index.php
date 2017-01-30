a1b49aad3245f8b65e5f80a2a3d55cce42d9b206
Stefan Hannibal <hannibalstefan@gmail.com>
2017-01-30 22:18:00 +0100

    added index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>training block</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Abel" />
+    </head>
+    <body>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/list.js"></script>
+        <button type="button" onclick="test()">Try it</button>
+        <div class="test"></div>
+
+    </body>
+</html>
