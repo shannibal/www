4ab1160265f9e4681ce056a60eba8e5b8a0742e2
Stefan Hannibal <hannibalstefan@gmail.com>
2018-05-24 00:12:24 +0200

    added google materialize

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
         <title>training block</title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Abel" />
-    </head>
-    <body>
         <script src="js/jquery-3.1.1.min.js"></script>
         <script src="js/list.js"></script>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</head>
+    <body onLoad="init()">
         <button type="button" onclick="test()">Try it</button>
         <button type="button" onclick="myFunction()">Test</button>
         <div class="test"></div>
-
     </body>
 </html>
