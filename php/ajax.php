ed23a86e80025b93e8f09e038d9566746e0aa3d3
Stefan Hannibal <hannibalstefan@gmail.com>
2018-05-24 00:12:45 +0200

    multiple queries

--- php/ajax.php
+++ php/ajax.php
@@ -1,23 +1,28 @@
 <?php
     require_once('db.php');
-
-    if ($_GET['func'] == 'get_macro') {
-        $macro = query("
-            SELECT macro_cycle.id as macro_id, macro_cycle.name as macro_name,
-            meso_cycle.id as meso_id, meso_cycle.name as meso_name, 
-            micro_cycle.id as micro_id, micro_cycle.name as micro_name
-            FROM macro_cycle 
-            LEFT JOIN meso_cycle ON meso_cycle.parent = macro_cycle.id 
-            LEFT JOIN micro_cycle ON micro_cycle.parent = meso_cycle.id 
-        ");
-        $array = array_merge();
-        echo json_encode($cycle);
-
-    } elseif ($_GET['func'] == 'test') {
-        $cycle = query("
-            SELECT exercise.id as exercise_id, exercise.name as exercise_name, exercise.parent as cycle_id
+    $array = array(
+        "macro" => query("
+            SELECT macro_cycle.id as id, macro_cycle.name as name
+            FROM macro_cycle
+        "),
+        "meso" => query("
+            SELECT meso_cycle.id as id, meso_cycle.name as name, meso_cycle.parent as parent
+            FROM meso_cycle
+        "),
+        "micro" => query("
+            SELECT micro_cycle.id as id, micro_cycle.name as name, micro_cycle.parent as parent
+            FROM micro_cycle
+        "),
+        "exercise" => query("
+            SELECT exercise.id as id, exercise.name as name, exercise.parent as parent
             FROM exercise
-        "); 
-        echo json_encode($cycle);
-    }
+        "),
+        "set" => query("
+            SELECT `set`.id as id, `set`.repetition as repetition, `set`.parent as parent, `set`.percent as percent, `set`.value as value
+            FROM `set`
+        ")
+    );
+        
+    echo json_encode($array);
+
 ?>
