0d17b2c3ade9c3542a6287689073c38076770400
Stefan Hannibal <hannibalstefan@gmail.com>
2018-06-12 18:25:09 +0200

    playing with layers

--- js/canvas.js
+++ js/canvas.js
@@ -9,29 +9,35 @@ $(document).ready( function() {
         x: 20, y: 20,
         radius: 20
     });
-    $('canvas').drawText({
-        layer: true,
-        name: 'text',
-        fillStyle: 'red',
-        x: 12, y: 24,
-        fontSize: 12,
-        fontFamily: 'Verdana, sans-serif',
-        text: ''
-    });
 
+    for (var i = 1; i < 25; i++) {
+        $('canvas').drawText({
+            layer: true,
+            name: `text${i}`,
+            fillStyle: 'red',
+            x: 22, y: i * 22,
+            fontSize: 18,
+            fontFamily: 'Verdana, sans-serif',
+            text: 'x',
+            click: function(layer) {
+                $(this).animateLayer(layer, {
+                    rotate: 0
+                });
+            }
+        });
+    }
     setInterval( function() {
-        var value = ((new Date - start) / 1000).toFixed(0);
-        $('canvas').setLayer('myBox', {
-            fillStyle: `rgb(${value}, ${value * 2}, ${value / 2})`,
-            start: "+=3.6"
+        var value = ~~((new Date - start) / 1000);
+        $('canvas').setLayer(value, {
+            fillStyle: `rgb(${value * 10}, +=5, 127)`,
+            text: `${value}`
         }).drawLayers();
-        $('canvas').setLayer('text', {
-            fillStyle: `rgb(${value*2}, 0, 0)`,
-            text: `${value*2}`
-        }).drawLayers();
-        if (value == 255) {
+        $('canvas').animateLayer(value, {
+            rotate: '+=100'
+        });
+        if (value == 25) {
             start = new Date;
         }
-    }, 100);
+    });
 
 });
