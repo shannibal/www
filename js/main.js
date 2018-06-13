9efe173de80e43c92382a72678f12920dfb7f4ef
Stefan Hannibal <hannibalstefan@gmail.com>
2018-06-13 23:19:49 +0200

    changed class slightly

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(document).ready( function() {
+    createExercises();
+
+    $('.btn.stop').click( function() {
+        $.data(document.body, "micro", 1);
+        createExercises();
+    });
+
+    $('.btn.start').click( function() {
+        $.data(document.body, "micro", 2);
+        createExercises();
+    });
+});
+
+function createExercises() {
+    $(".exercises").empty();
     $.ajax({
         type: "get", dataType: "json",
         cache: false, async: true,
@@ -6,24 +21,21 @@ $(document).ready( function() {
         data: {'func': 'exercise'},
         success: function(response) {
             $.each(response, function(key, value) {
-                if (value.set_parent != 1) {
-                    return true;
-                }
-                if (value.set_parent != 1) {
+                if (value.set_parent != $.data(document.body, "micro")) {
                     return true;
                 }
                 if (value.set_id != null) {
                     //console.log(value);
-                    if (!$(`.exercise_${value.exercise_name}`).length) {
+                    if (!$(`#${value.exercise_name}.exercise`).length) {
                         console.log("create", value.exercise_name);
-                        $(`.exercise`).append(`<div class="row exercise_${value.exercise_name}"><div class="col ${value.exercise_name} s12">${value.exercise_name}</div></div>`);
+                        $(`.exercises`).append(`<div class="row exercise" id="${value.exercise_name}"><div class="col ${value.exercise_name} s12">${value.exercise_name}</div></div>`);
                         // $(`.exercise`).append(`<div class="row exercise ${value.exercise_name}"><div class="col s12">${value.exercise_name}</div></div>`);
                     } else {
                         //console.log("exists", value.exercise_name);
                     }
                     
                     console.log("add", value.exercise_name);
-                    $(`.exercise_${value.exercise_name}`).append(`
+                    $(`#${value.exercise_name}.exercise`).append(`
                         <div class="row">
                             <div class="col s3">${value.set_id}</div>
                             <div class="col s3">&nbsp;</div>
@@ -32,12 +44,13 @@ $(document).ready( function() {
                         </div>
                     `);
                     
-
                 }
             });
+            // $(".exercises").last().css("background-color", "pink" );
         },
         error: function(response) {
             console.log(response);
         }
     });
-});
+
+}
