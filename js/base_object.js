11eb8dbe82fda963915a6fdf9e44e23e73cfa8ad
Stefan Hannibal <hannibalstefan@gmail.com>
2018-06-16 22:06:34 +0200

    added object

--- js/base_object.js
+++ js/base_object.js
@@ -1,9 +1,66 @@
-class Test {
-  constructor() {
-    this.name = "Polygon";
-  }
+var gym = {
+    __sets: {},
+    __exercises: {},
+    Exercise: 
+        class Exercise {
+            constructor(data) {
+                this.id = Number(data.exercise_id);
+                this.name = data.exercise_name;
+                this.parent = Number(data.exercise_parent);
+                this.pr = Number(data.exercise_pr);
+                this.elem;
+                gym.__exercises[this.id] = this;
+            }
+        },
+
+    Set:
+        class Set {
+            constructor(data) {
+                this.id = Number(data.set_id);
+                this._exercise = Number(data.set_exercise);
+                this.percent = Number(data.set_percent);
+                this.parent = Number(data.set_parent);
+                this.repetition = Number(data.set_repetition);
+                this.value = Number(data.set_value);
+                gym.__sets[this.id] = this;
+            }
+    },
+
+    get_exercises: function() {
+        return gym.__exercises;
+    },
+
+    get_sets: function() {
+        return gym.__sets;
+    }
+};
+
+gym.Exercise.prototype.toString = function() {
+    return `
+        <div class="row exercise">
+            <div class="col s12 exercise_top" id="${this.name}">${this.name}</div>
+        </div> 
+    `;
 }
 
-Test.prototype.speak = function() {
+gym.Set.prototype.toString = function() {
+    return `
+        <div class="row set">
+            <div class="col s3">${this.id}</div>
+            <div class="col s3">&nbsp;</div> 
+            <div class="col s3">${this.value} x ${this.repetition}</div> 
+            <div class="col s3">${((this.percent/100) * this.exercise.pr).toFixed(0)} kg</div>
+        </div>
+    `;
+}
+
+
+Object.defineProperty(gym.Set.prototype, "exercise", {
+    get: function() {
+        return (this._exercise in gym.__exercises) ? gym.__exercises[this._exercise] : this._exercise;
+    }
+});
+
+gym.Exercise.prototype.speak = function() {
   return this;
 }
--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 $.getScript("/js/base_object.js", function() {
-   $.data(document.body, "test", new Test());
+    console.log(gym.get_exercises());
+   // $.data(document.body, "test", new Test());
 });
 
 $(document).ready( function() {
     $('.sidenav').sidenav();
-
-    createExercises();
+    $.ajax({
+        type: "get", dataType: "json",
+        cache: false, async: true,
+        url: "/php/ajax.php",
+        data: {'func': 'exercise'},
+        success: function(response) {
+            $.each(response, function(key, value) {
+                if (!(value.exercise_id in gym.get_exercises())) {
+                    var exercise = new gym.Exercise(value);
+                }
+                var set = new gym.Set(value);
+                console.log(set.toString())
+                // console.log(set, set.exercise);
+            });
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
 
     $('.btn.stop').click( function() {
         $.data(document.body, "micro", 1);
@@ -20,41 +38,21 @@ $(document).ready( function() {
 
 function createExercises() {
     $(".exercises").empty();
-    $.ajax({
-        type: "get", dataType: "json",
-        cache: false, async: true,
-        url: "/php/ajax.php",
-        data: {'func': 'exercise'},
-        success: function(response) {
-            $.each(response, function(key, value) {
-                if (value.set_parent != $.data(document.body, "micro")) {
-                    return true;
-                }
-                if (value.set_id != null) {
-                    //console.log(value);
-                    if (!$(`#${value.exercise_name}.exercise`).length) {
-                        console.log("create", value.exercise_name);
-                        $(`.exercises`).append(`<div class="row exercise" id="${value.exercise_name}"><div class="col ${value.exercise_name} s12">${value.exercise_name}</div></div>`);
-                        // $(`.exercise`).append(`<div class="row exercise ${value.exercise_name}"><div class="col s12">${value.exercise_name}</div></div>`);
-                    } else {
-                        //console.log("exists", value.exercise_name);
-                    }
-                    
-                    console.log("add", value.exercise_name);
-                    $(`#${value.exercise_name}.exercise`).append(`
-                        <div class="col s3">${value.set_id}</div>
-                        <div class="col s3">&nbsp;</div>
-                        <div class="col s3">${value.set_value} x ${value.set_repetition}</div>
-                        <div class="col s3">${((value.set_percent/100) * value.exercise_pr).toFixed(0)} kg</div>
-                    `);
-                    
-                }
-            });
-            // $(".exercises").last().css("background-color", "pink" );
-        },
-        error: function(response) {
-            console.log(response);
-        }
+
+    $.each(gym.get_exercises(), function() {
+        // var elem = $(".exercises").append(this.toString());
+        var elem = $(this.toString()).appendTo(".exercises");
+        this.elem = elem;
+        console.log(">", elem);
+    });
+
+    $.each(gym.get_sets(), function() {
+        this.exercise.elem.append(this.toString());
+        // $(`#${this.exercise.name}`).append(this.toString());
+    });
+
+    $.each(gym.get_exercises(), function() {
+        this.elem.append('<div class="col s12 exercise_new"><a class="btn">Add</a><div/>');
     });
 
 }
