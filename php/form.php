b9db2564c5fc1e0abdeed496c35ebd4953277212
Stefan Hannibal <hannibalstefan@gmail.com>
2018-06-17 01:05:03 +0200

    remove py and php

--- php/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "<form>";
-    echo "<button type='button'>+</button>";
-    echo "<input type='text'>";
-    echo "<input type='number' name='procent' maxlength='1'>";
-    echo "<input type='number' name='sets' min='1' max='99'>";
-    echo "<input type='number' name='reps' min='1' max='99'>";
-    echo "</form>";
-?>
